27e7e7c3d95a7dbc3abb459a50d5af73f300a9e0
lopress <lola.pressler05@htl-salzburg.ac.at>
2022-01-27 10:20:00 +0100

    mainSceneControllerUpdate

--- Assets/MyGame/Scripts/MainSceneController.cs
+++ Assets/MyGame/Scripts/MainSceneController.cs
@@ -20,25 +20,38 @@ public class MainSceneController : MonoBehaviour
 
     public void GetValues()
     {
-        string termA, termB;
-        termA = inputFieldTermA.text;
-        termB = inputFieldTermB.text;
+        //string termA, termB;
+        //termA = inputFieldTermA.text;
+        //termB = inputFieldTermB.text;
 
-        Debug.Log("Methode GetValues wird ausgeführt: " + CheckAddition(int.Parse(termA), int.Parse(termB)));
+        //Debug.Log("Methode GetValues wird ausgeführt: " + CheckAddition(int.Parse(termA), int.Parse(termB)));
+
+
+        //holen der Werte
+        try
+        {
+            termA = int.Parse(inputFieldTermA.text);
+        }
+        catch (System.Exception)
+        {
+
+            inputFieldTermA.GetComponent<InputField>().image.color = Color.red;
+            inputFieldTermA.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "Bitte gültige Zahl eingeben!";
+            inputFieldTermA.text = "";
+        }
+
+        termB = int.Parse(inputFieldTermB.text);
+
+        txtResult.text = CheckAddition(termA, termB).ToString();
     }
 
     public void ResetBtn()
     {
-        inputFieldTermA.text = string.Empty;
-        inputFieldTermB.text = string.Empty;
+        inputFieldTermA.text = "";
+        inputFieldTermB.text = "";
         txtResult.text = "";
 
         termA = 0;
         termB = 0;
-    }
-
-    private void Start()
-    {
-        Debug.Log("Frohe Weihnachten");
     }
 }
